ca395d8cba6ce835aab5006ef15caa0086e95a0e
Sergey Koksharov <gentoosiastic@gmail.com>
2023-01-01 11:22:29 +0300

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "cats_data.json"));
+const middlewares = jsonServer.defaults({
+  readOnly: true,
+});
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
